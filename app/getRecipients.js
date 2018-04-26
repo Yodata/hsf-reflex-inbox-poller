a1af0fbe2b1c5ed2a7d1953db344028be0d7fa3c
Dave Duran <dave@yodata.io>
2018-04-26 11:27:06 -0700

    fix getRecipient not handling single object use case.

--- app/getRecipients.js
+++ app/getRecipients.js
@@ -1,29 +1,33 @@
-const get = require('lodash.get')
-const castArray = require('lodash.castarray')
+const get = require("lodash.get");
+const castArray = require("lodash.castarray");
 
-const inboxURL = (recipient) => {
-  let id
-  if (typeof recipient === 'string') {
-    id = recipient
+const inboxURL = recipient => {
+  let id;
+  if (typeof recipient === "string") {
+    id = recipient;
   } else {
-    id = recipient && recipient.id || recipient['@id']
+    id = (recipient && recipient.id) || recipient["@id"];
   }
-  return id && id.replace('/profile/card#me', '/inbox/').toLowerCase()
-}
+  return id && id.replace("/profile/card#me", "/inbox/").toLowerCase();
+};
 
-function getRecipients (message) {
-  let result = []
+function getRecipients(message) {
+  let result = [];
   if (message && message.recipient) {
-    if (typeof message.recipient === 'string') {
-      result.push(inboxURL(message.recipient))
+    if (
+      typeof message.recipient === "string" ||
+      message.recipient.id ||
+      message.recipient["@id"]
+    ) {
+      result.push(inboxURL(message.recipient));
     } else if (Array.isArray(message.recipient)) {
       message.recipient.forEach(recipient => {
-        let url = inboxURL(recipient)
-        if (url) result.push(url)
-      })
+        let url = inboxURL(recipient);
+        if (url) result.push(url);
+      });
     }
   }
-  return result
+  return result;
 }
 
-module.exports = getRecipients
+module.exports = getRecipients;
